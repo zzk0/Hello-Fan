514a783d4a7336571eceb72be901ee3e83403be3
DrXiang <DrXinhuanan@163.com>
2019-05-30 00:09:37 +0800

    1.添加main->assets目录存放sql文件； 2.Hello_Fan.sql下面有两个表： 	字典：CREATE TABLE IF NOT EXISTS dict (ID INT PRIMARY KEY, words VARCHAR, spell TEXT, express VARCHAR); 	简繁对照：CREATE TABLE IF NOT EXISTS words (ID INT PRIMARY KEY, traditional VARCHAR, simplified VARCHAR, value INT);                   (跟群里发的words.sql有一点点不一样，不影响的（大概）...) 3.DBHelper类负责读取sql文件创建数据库； ....

--- app/src/main/java/com/test/fan/MainActivity.java
+++ app/src/main/java/com/test/fan/MainActivity.java
@@ -5,14 +5,12 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -42,12 +40,16 @@ public class MainActivity extends AppCompatActivity
     private static final int SETTING = 5;
     private static final int READ_WRITE_PERM = 2333;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
