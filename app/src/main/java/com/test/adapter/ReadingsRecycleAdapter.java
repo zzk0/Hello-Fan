d2d2706a16da1c323eafdfba0d4190fc954ff038
Linchen <2521977434@qq.com>
2019-06-09 07:55:24 +0800

    补全了缺少的代码

---
+++ app/src/main/java/com/test/adapter/ReadingsRecycleAdapter.java
@@ -0,0 +1,178 @@
+package com.test.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.test.fan.R;
+import com.test.fan.ReadingsDisplayActivity;
+import com.test.model.Readings;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ReadingsRecycleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final int IMAGE_VIEW=0,NO_IMAGE_VIEW=1;
+    //当前上下文对象
+    private Context context;
+    //RecyclerView填充Item数据的List对象
+    private List<Readings> readingsList;
+    private Set<String> urlSet=new HashSet<String>();
+    public ReadingsRecycleAdapter(Context context,List<Readings> readingsList){
+        this.context = context;
+        this.readingsList = readingsList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(readingsList.get(position).getImg_url().equals(""))
+            return NO_IMAGE_VIEW;
+        else
+            return IMAGE_VIEW;
+    }
+
+    //创建ViewHolder
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //实例化得到Item布局文件的View对象
+        View view;
+        if(viewType==IMAGE_VIEW) {
+            view = LayoutInflater.from(context).inflate(R.layout.readings_item,
+                    null);
+            return new ImageViewHolder(view);
+        }
+        else
+        {
+            view= LayoutInflater.from(context).inflate(R.layout.readings_no_image_item,
+                    null);
+            return new NoImageViewHolder(view);
+
+        }
+        //返回MyViewHolder的对象
+
+    }
+    //绑定数据
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Readings readings= readingsList.get(position);
+        if(holder instanceof ImageViewHolder)
+        {
+            ImageViewHolder imageHolder=(ImageViewHolder)holder;
+            if(urlSet.contains(readings.getUrl())) {
+                imageHolder.readings_title.setTextColor(Color.GRAY);
+            }
+            imageHolder.readings_title.setText(readings.getTitle());
+            imageHolder.readings_digest.setText(readings.getDigest());
+            if(!readings.getGroup().equals(""))
+                imageHolder.readings_group.setText(readings.getGroup()+"   ");
+            imageHolder.readings_date.setText(readings.getDate());
+            if(!readings.getImg_url().equals(""))
+                Glide.with(context).load(readings.getImg_url()).diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(imageHolder.readings_imageView);
+        }
+        else if(holder instanceof NoImageViewHolder)
+        {
+            NoImageViewHolder noImageHolder=(NoImageViewHolder)holder;
+            if(urlSet.contains(readings.getUrl())) {
+                noImageHolder.readings_title.setTextColor(Color.GRAY);
+            }
+            noImageHolder.readings_title.setText(readings.getTitle());
+            noImageHolder.readings_digest.setText(readings.getDigest());
+            if(!readings.getGroup().equals(""))
+                noImageHolder.readings_group.setText(readings.getGroup());
+            noImageHolder.readings_date.setText(readings.getDate());
+        }
+    }
+
+    //返回Item的数量
+    @Override
+    public int getItemCount() {
+        return readingsList.size();
+    }
+
+    //继承RecyclerView.ViewHolder抽象类的自定义ViewHolder
+    class ImageViewHolder extends RecyclerView.ViewHolder {
+        TextView readings_title;
+        ImageView readings_imageView;
+        TextView readings_digest;
+        TextView readings_date;
+        TextView readings_group;
+        public ImageViewHolder(View view) {
+            super(view);
+            readings_title = (TextView) view
+                    .findViewById(R.id.readings_title);
+            readings_imageView =(ImageView)view.findViewById(R.id.imageView);
+            readings_digest =(TextView) view.findViewById(R.id.readings_content);
+            readings_date=(TextView)view.findViewById(R.id.date);
+            readings_group=(TextView)view.findViewById(R.id.group);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Readings readings=readingsList.get(getLayoutPosition());
+                    urlSet.add(readings.getUrl());
+                    Intent intent = new Intent(context, ReadingsDisplayActivity.class);
+                    intent.putExtra("readings", readings);
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+    }
+    //继承RecyclerView.ViewHolder抽象类的自定义ViewHolder
+    class NoImageViewHolder extends RecyclerView.ViewHolder {
+        TextView readings_title;
+        TextView readings_digest;
+        TextView readings_group;
+        TextView readings_date;
+        public NoImageViewHolder(View view) {
+            super(view);
+            readings_title = (TextView) view
+                    .findViewById(R.id.readings_title);
+            readings_digest =(TextView) view.findViewById(R.id.readings_content);
+            readings_date=(TextView)view.findViewById(R.id.date);
+            readings_group=(TextView)view.findViewById(R.id.group);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Readings readings=readingsList.get(getLayoutPosition());
+                    urlSet.add(readings.getUrl());
+                    Intent intent = new Intent(context, ReadingsDisplayActivity.class);
+                    intent.putExtra("readings", readings);
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+    }
+    //下面两个方法提供给页面刷新和加载时调用
+    public void load(List<Readings> addReadingsList) {
+        //增加数据
+        int position = getItemCount();
+        readingsList.addAll(position, addReadingsList);
+        notifyItemInserted(position);
+    }
+
+    public void recommend(List<Readings> newList) {
+        //推荐数据
+        readingsList.addAll(0,newList);
+        notifyDataSetChanged();
+    }
+    public void refresh(List<Readings> newList)
+    {
+        //刷新数据
+        readingsList.clear();
+        readingsList.addAll(newList);
+        notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/test/fan/ReadingsDisplayActivity.java
@@ -0,0 +1,191 @@
+package com.test.fan;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.test.model.Readings;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class ReadingsDisplayActivity extends AppCompatActivity {
+    private View toast_view;
+    private Dialog mBottomDialog;
+    private Handler handler;
+    private TextView title_tv;
+    private Readings readings;
+    private ImageView imageView;
+    private TextView content_tv;
+    private TextView group_tv;
+    private TextView date_tv;
+    private View view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_readingsdisplay);
+        toast_view=View.inflate(this,R.layout.toast_view,null);
+        title_tv =(TextView)findViewById(R.id.title);
+        content_tv =(TextView)findViewById(R.id.content);
+        imageView=(ImageView)findViewById(R.id.imageView);
+        group_tv=(TextView)findViewById(R.id.group);
+        date_tv=(TextView)findViewById(R.id.date);
+        readings =(Readings) getIntent().getSerializableExtra("readings");
+        System.out.println(readings.getUrl());
+        initDisplay();
+
+    }
+    private void setGetWordListener()
+    {
+        SpannableStringBuilder s = new SpannableStringBuilder(readings.getContent());
+        for(int i=0;i<s.length();i++){
+            s.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(View v) {
+                }
+                @Override
+                public void updateDrawState(TextPaint ds) {
+                    super.updateDrawState(ds);
+                    ds.setColor(0xff000000);       //设置字体颜色
+                    ds.setUnderlineText(false);      //设置下划线
+                }
+            },i,i+1, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        }
+        //设置textview中所有文字为spannable格式
+        content_tv.setText(s,TextView.BufferType.SPANNABLE);
+        //启用上面为每个字绑定的ClickableSpan
+        content_tv.setMovementMethod(LinkMovementMethod.getInstance());
+        final Context context=this;
+        content_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //若没有绑定clickableSpan，无法使用subSequence方法
+                //若tv.getSelectionStart()-1,则输出点击的文字以及其上一个文字
+                //若tv.getSelectionEnd()+1,则输出点击的文字以及其下一个文字，如此类推
+                //通过标点判断还可截取一段文字中我们所点击的那句话
+                TextView tv = (TextView) v;
+                String s = tv
+                        .getText()
+                        .subSequence(tv.getSelectionStart()-1,
+                                tv.getSelectionEnd()).toString();
+                if(mBottomDialog==null)
+                    initBottomDialog();
+                updateDate(s);
+                if(!mBottomDialog.isShowing())
+                    mBottomDialog.show();
+            }
+        });
+
+    }
+    private void initDisplay()
+    {
+        if(!readings.getGroup().equals(""))
+            group_tv.setText(readings.getGroup()+"    ");
+        date_tv.setText(readings.getDate());
+        title_tv.setText(readings.getTitle());
+        if(!readings.getImg_url().equals(""))
+        {
+            Glide.with(this).load(readings.getImg_url()).diskCacheStrategy(DiskCacheStrategy.RESOURCE).into( imageView );
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getReadingsContent(readings.getUrl());
+            }
+        }).start();
+        handler=new Handler() {
+            public void handleMessage(Message msg) {
+                if (msg.what == 1) {
+                    setGetWordListener();
+                }
+            }
+        };
+    }
+    private void showToast(String s)
+    {
+        Toast toast=new Toast(this);
+        TextView toast_tv=(TextView)toast_view.findViewById(R.id.toast_tv);
+        toast_tv.setText(s);
+        toast.setView(toast_view);
+        toast.setGravity(Gravity.BOTTOM,0,75);
+        toast.show();
+    }
+    private void getReadingsContent( String content_url)
+    {
+
+                Document content= null;
+                try {
+                    content = Jsoup.connect(content_url).get();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String s=content.select("div.TRS_Editor>p[align=justify],div.TRS_Editor>div>p[align=justify]").text();
+                if(s.equals(""))
+                    s=content.select("div.TRS_Editor>p,div.TRS_Editor>div>p").text();
+                s=s.replace(" 　　","\n 　　");
+                readings.setContent(s);
+                Message msg=new Message();
+                msg.what = 1;
+                handler.sendMessage(msg);
+    }
+    /**
+     * 显示分享弹出框
+     */
+    private void updateDate(String s)
+    {
+        TextView word_tv=(TextView)view.findViewById(R.id.word);
+        TextView info_tv=(TextView)view.findViewById(R.id.word_info);
+        word_tv.setText(s);
+        info_tv.setText(s);
+    }
+    /**
+     * 初始化分享弹出框
+     */
+
+    private void initBottomDialog() {
+        mBottomDialog = new Dialog(this, R.style.dialog_bottom_full);
+        mBottomDialog.setCanceledOnTouchOutside(false);
+        Window window = mBottomDialog.getWindow();
+        window.setGravity(Gravity.BOTTOM);
+        window.setWindowAnimations(R.style.share_animation);
+        view = View.inflate(this, R.layout.dialog_fan, null);
+        view.findViewById(R.id.tv_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBottomDialog != null && mBottomDialog.isShowing()) {
+                    mBottomDialog.dismiss();
+                }
+            }
+        });
+        final Context context=this;
+        view.findViewById(R.id.collect_tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("已成功收藏");
+            }
+        });
+        window.setContentView(view);
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);//设置横向全屏
+    }
+
+}
